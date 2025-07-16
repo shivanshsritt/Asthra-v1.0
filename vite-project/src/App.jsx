d8a7a543b713f0f77b856ed71142de6dfdec51fc
shivansh srivastava <catkelvis@gmail.com>
2025-07-17 00:24:00 +0530

    first commit just intizlise the prject

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Mera from './Mera'
+
+
+
+function App() {
+  return (
+    <>
+  
+  <Mera/>
+      <h1>Vite + React</h1>
+
+      <p className="read-the-docs">
+
+        learning vite bro🤣</p> 
+    </>
+  )
+}
+
+
+export default App
---
+++ vite-project/src/mera.jsx
@@ -0,0 +1,13 @@
+
+
+function Mera () {
+return (< >
+
+
+<h1> HELLO </h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro tempore cumque sed officia necessitatibus. Libero voluptatum porro voluptatibus modi dolores asperiores eveniet, labore sit non nam odit omnis nulla sint.
+
+</>)
+
+}
+
+export default Mera
